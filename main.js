f392e7ba6de1d1ee5f7b5dc5b200986dac6208aa
Julian Atkin <jmd.atkin@gmail.com>
2020-07-03 08:20:09 +0100

    added images and improved styling

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 window.onload = function() {
     var titleBar = document.querySelector(".retro-title-bar");
-    var content = document.querySelector(".main-content-container");
+    var windowContainer = document.querySelector(".window-container");
+    var content = document.querySelector(".window-content");
     var contentViewer = document.querySelector(".content-viewer");
 
+    //Object for storing window position
     var pos = (function() {
 
         var obj = {
@@ -10,14 +12,15 @@ window.onload = function() {
             y: 0
         };
 
+        //Setting the object's value changes the css position
         var set = function(data) {
             Object.keys(data).forEach(function(val) {
                 if (obj.hasOwnProperty(val))
                     obj[val] = data[val];
             });
-            content.style.position = "absolute"
-            content.style.left = obj.x + "px";
-            content.style.top = obj.y + "px";
+            windowContainer.style.position = "absolute"
+            windowContainer.style.left = obj.x + "px";
+            windowContainer.style.top = obj.y + "px";
         };
 
         var get = function() {
@@ -36,58 +39,82 @@ window.onload = function() {
         };
     })();
 
-    var bindMouseEvents = function() {
-        var handler;
-        var mouseDownEvt = function(e) {
-            if (content.style.position != "absolute") {
-                pos.set({
-                    x: content.getBoundingClientRect().left,
-                    y: content.getBoundingClientRect().top
-                });
-                content.style.position = "absolute";
-            }
+    var bindEvents = function() {
+        
+        var bindWindowEvents = function() {
+            var handler;
+            var mouseDownEvt = function(e) {
+                if (windowContainer.style.position != "absolute") {
+                    pos.set({
+                        x: windowContainer.getBoundingClientRect().left,
+                        y: windowContainer.getBoundingClientRect().top
+                    });
+                    windowContainer.style.position = "absolute";
+                }
 
-            titleBar.style.cursor = "grabbing";
-            contentViewer.style.pointerEvents = "none";
+                titleBar.style.cursor = "grabbing";
+                contentViewer.style.pointerEvents = "none";
 
-            var slice = pos.get();
-            handler = doMousedown(slice, e.clientX, e.clientY);
-        };
-
-        var doMousedown = function(slice, ix, iy) {
-            var mouseMoveEvt = function(e) {
-                var diffX = e.clientX - ix;
-                var diffY = e.clientY - iy;
-
-                console.log(diffX);
-
-                pos.set({
-                    x: slice.x + diffX,
-                    y: slice.y + diffY
-                });
+                var slice = pos.get();
+                handler = doMousedown(slice, e.clientX, e.clientY);
             };
-            document.body.addEventListener("mousemove", mouseMoveEvt);
-            return mouseMoveEvt;
+
+            var doMousedown = function(slice, ix, iy) {
+                var mouseMoveEvt = function(e) {
+                    var diffX = e.clientX - ix;
+                    var diffY = e.clientY - iy;
+
+                    console.log(diffX);
+
+                    pos.set({
+                        x: slice.x + diffX,
+                        y: slice.y + diffY
+                    });
+                };
+                document.body.addEventListener("mousemove", mouseMoveEvt);
+                return mouseMoveEvt;
+            };
+
+            var mouseUpEvt = function() {
+                if (handler !== undefined)
+                    document.body.removeEventListener("mousemove", handler);
+                titleBar.style.cursor = "grab";
+                contentViewer.style.pointerEvents = "auto";
+            };
+
+            titleBar.addEventListener("mousedown", mouseDownEvt);
+            document.body.addEventListener("mouseup", mouseUpEvt);
         };
 
-        var mouseUpEvt = function() {
-            if (handler !== undefined)
-                document.body.removeEventListener("mousemove", handler);
-            titleBar.style.cursor = "grab";
-            contentViewer.style.pointerEvents = "auto";
+        var bindButtonEvents = function() {
+            //Close button
+            document.querySelector("#close").onclick = function() {
+                if (windowContainer.classList.contains("hidden"))
+                    windowContainer.classList.remove("hidden");
+                else
+                    windowContainer.classList.add("hidden");
+            };
+
+            document.querySelector("#shutter").onclick = function() {
+                if (content.classList.contains("hidden"))
+                    content.classList.remove("hidden");
+                else
+                    content.classList.add("hidden");
+
+            };
         };
 
-        titleBar.addEventListener("mousedown", mouseDownEvt);
-        document.body.addEventListener("mouseup", mouseUpEvt);
-
-        //Close button
-        document.querySelector("#close").onclick = function() {
-            if (content.classList.contains("hidden"))
-                content.classList.remove("hidden");
-            else
-                content.classList.add("hidden");
+        var bindDesktopIconEvents = function() {
+            document.querySelector("#open-window").onclick = function() {
+                if (windowContainer.classList.contains("hidden"))
+                    windowContainer.classList.remove("hidden");
+            };
         };
+
+        bindWindowEvents();
+        bindButtonEvents();
+        bindDesktopIconEvents();
     };
 
-    bindMouseEvents();
+    bindEvents();
 };
