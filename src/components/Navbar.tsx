e92cc58d159aa4dd917d28a68491ba88d95a0761
Julian Atkin <jatkindev@gmail.com>
2023-03-04 03:49:56 -0500

    Improve nav link hitboxes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -27,12 +27,12 @@ function Navbar() {
 
 
     return (
-        <nav className="h-full flex items-center px-8 justify-around md:justify-start space-x-4">
+        <nav className="h-full flex items-center px-3 justify-around md:justify-start">
             {links.current.map((link, idx) => {
                 // const active = router.pathname === link.href;
                 const re = new RegExp(`^${link.href}(/.+)?$`);
                 const active = re.test(router.pathname);
-                return <Link key={idx} className={`${active ? 'text-zinc-900' : ''} hover:text-zinc-900 font-semibold text-zinc-400 duration-[0.07s]`} href={link.href}>{link.name}</Link>
+                return <Link key={idx} className={`${active ? 'text-zinc-900' : ''} block p-4 grow md:grow-0 h-full flex items-center hover:text-zinc-900 font-semibold text-zinc-400 duration-[0.07s]`} href={link.href}>{link.name}</Link>
             })}
         </nav>
     );
